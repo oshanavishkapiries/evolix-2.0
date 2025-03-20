53a3af5c96bbcd2313db3b9deec115edd85cfbe2
OSHAN AVISHKA <153918928+oshanavishkapiries@users.noreply.github.com>
2025-03-20 19:33:48 +0530

    evolix-web-done

---
+++ evolix-web/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the root directory
+app.use(express.static(__dirname));
+
+// Serve index.html for the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+}); 
